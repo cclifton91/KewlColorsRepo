df7ef8dd786d57acdabc87fae9dd3a2ee8e45e7b
Camille Clifton <47505531+cclifton91@users.noreply.github.com>
2020-08-27 09:09:00 -0500

    filddle around with image plugins

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,7 @@
 const pluginPWA = require("eleventy-plugin-pwa");
 const cleanCSS = require("clean-css");
-const responsivePicturePlugin = require("eleventy-plugin-responsive-picture");
 const pluginSEO = require("eleventy-plugin-seo");
+
 
 module.exports = function (config) {
 	/**Plugins **/
@@ -16,42 +16,15 @@ module.exports = function (config) {
 	config.addFilter("cssmin", function (code) {
 		return new cleanCSS({}).minify(code).styles;
 	});
-	//Responsive Picture
-	config.addPlugin(responsivePicturePlugin, {
-		ratios: [2, 1],
-		sources: [{
-				media: "(min-width: 1024px)",
-				size: 824
-			},
-			{
-				media: "(min-width: 768px)",
-				size: 696
-			},
-			{
-				media: "(min-width: 420px)",
-				size: 568
-			},
-			{
-				size: 348
-			},
-		],
-		fallback: (src) => `${src}?w=1000`,
-		resize: (src, width) => `${src}?w=${size}`,
-	});
+	//Responsive Image
+	
 	//SEO
-	config.addPlugin(pluginSEO, {
-		title: "Kewl Colors",
-		description: "Hand painted, custom made accessories",
-		url: "https://kewlcolors.com",
-		author: "Kari Wilson",
-		tiwtter: "kariwlison",
-		image: "./assets/images/32.png",
-		ogtype: "website",
-	});
+	config.addPlugin(pluginSEO, require("./src/_data/seo.json"));
 
 	/**Eleventy Configuration **/
 	config.addPassthroughCopy("manifest.json");
-	config.addPassthroughCopy("./assets/images");
+	config.addPassthroughCopy({ "src/_assets/img": "assets/img" });
+	config.addPassthroughCopy({ "src/_assets/icons": "assets/icons" });
 	config.addPassthroughCopy("style-guide");
 	config.addPassthroughCopy("admin");
 	return {
