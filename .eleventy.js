826d8297c4984c6e7892cbc72636bb235ef2b7b8
Camille Clifton <47505531+cclifton91@users.noreply.github.com>
2020-08-31 17:01:03 -0500

    added eleventy-img and optomized images

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,7 @@
 const pluginPWA = require("eleventy-plugin-pwa");
 const cleanCSS = require("clean-css");
 const pluginSEO = require("eleventy-plugin-seo");
-const localImages = require('eleventy-plugin-local-images');
+const Image = require("@11ty/eleventy-img");
 
 module.exports = function (config) {
 	/**Plugins **/
@@ -17,12 +17,71 @@ module.exports = function (config) {
 		return new cleanCSS({}).minify(code).styles;
 	});
 	//Responsive Image
-	config.addPlugin(localImages, {
-		distPath: "_site",
-		assetPath: "/assets/img",
-		selector: "img",
-		verbose: false,
+	async function optimImg(src, opts) {
+		const finalOpts = {
+			widths: [320, 640, 768, 1024, 1366, 1600, 1920],
+			formats: ["webp","png", "jpg"],
+			urlPath: "/assets/img",
+			outputDir: "./_site/assets/img",
+			...opts,
+		};
+		let stats = await Image(src, finalOpts);
+		if (finalOpts.widths.length > 1) return stats;
+		else return stats["jpg"].pop();
+	}
+	config.addNunjucksAsyncShortcode("image", async function (src, alt, options) {
+		if (alt === undefined) {
+			throw new Error(`Missing \`alt\` on resImage from: ${src}`);
+		}
+
+		let stats = await optimImg(src);
+		let lowestSrc = stats.jpg[0];
+		let sizes = "100vw, (min-width: 39.5rem) 608px";
+
+		// Iterate over formats and widths
+		return `<picture>
+			${Object.values(stats)
+				.map((imageFormat) => {
+				return `  <source type="image/${imageFormat[0].format}" srcset="${imageFormat
+					.map((entry) => `${entry.url} ${entry.width}w`)
+					.join(", ")}" sizes="${sizes}">`;
+				})
+				.join("\n")}
+				<img
+				alt="${alt}"
+				src="${lowestSrc.url}"
+				width="${lowestSrc.width}"
+				height="${lowestSrc.height}"
+				loading="lazy">
+      </picture>`;
 	});
+	config.addNunjucksAsyncShortcode("figure", async function (src, alt, caption, options) {
+		if (alt === undefined) {
+			throw new Error(`Missing \`alt\` on resImage from: ${src}`);
+		}
+
+		let stats = await optimImg(src);
+		let lowestSrc = stats.jpg[0];
+		let sizes = "100vw";
+
+		// Iterate over formats and widths
+		return `<figure><picture>
+      ${Object.values(stats)
+        .map((imageFormat) => {
+          return `  <source type="image/${imageFormat[0].format}" srcset="${imageFormat
+            .map((entry) => `${entry.url} ${entry.width}w`)
+            .join(", ")}" sizes="${sizes}">`;
+        })
+        .join("\n")}
+        <img
+          alt="${alt}"
+          src="${lowestSrc.url}"
+          width="${lowestSrc.width}"
+          height="${lowestSrc.height}"
+          loading="lazy">
+      </picture><figcaption>${caption}</figcaption></figure>`;
+	});
+	
 	//SEO
 	config.addPlugin(pluginSEO, require("./src/_data/seo.json"));
 
