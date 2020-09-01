82d31a4a667013db7e550511ceaa14332bd3629a
Camille Clifton <47505531+cclifton91@users.noreply.github.com>
2020-09-01 10:05:15 -0500

    unistall eleventy images

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,7 @@
 const pluginPWA = require("eleventy-plugin-pwa");
 const cleanCSS = require("clean-css");
 const pluginSEO = require("eleventy-plugin-seo");
-const Image = require("@11ty/eleventy-img");
+
 
 module.exports = function (config) {
 	/**Plugins **/
@@ -17,34 +17,7 @@ module.exports = function (config) {
 		return new cleanCSS({}).minify(code).styles;
 	});
 	//Responsive Image
-	config.addNunjucksAsyncShortcode("responsiveimg", async function (src, alt, options) {
-		if (alt === undefined) {
-			// You bet we throw an error on missing alt (alt="" works okay)
-			throw new Error(`Missing \`alt\` on responsiveimg from: ${src}`);
-		}
-
-		let stats = await Image(src, {
-			widths: [null],
-			formats: ["webp", "jpg", "png"],
-			urlPath: "/assets/img",
-			outputDir: "./_site/assets/img",
-			...options
-		});
-		let lowestSrc = stats.jpg[0];
-		let sizes = "(min-width: 64rem) 80vw,(min - width: 48rem) 90vw, 100vw "; // Make sure you customize this!
-
-		// Iterate over formats and widths
-		return `<picture>
-      ${Object.values(stats).map(imageFormat => {
-        return `  <source type="image/${imageFormat[0].format}" srcset="${imageFormat.map(entry => `${entry.url} ${entry.width}w`).join(", ")}" sizes="${sizes}">`;
-      }).join("\n")}
-        <img
-          src="${lowestSrc.url}"
-          width="${lowestSrc.width}"
-          height="${lowestSrc.height}"
-          alt="${alt}">
-      </picture>`;
-	});
+	
 	//SEO
 	config.addPlugin(pluginSEO, require("./src/_data/seo.json"));
 
