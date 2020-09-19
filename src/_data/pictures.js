10cd616f17e802098f07c9e7dfd26d1c4d939de4
Camille Clifton <47505531+cclifton91@users.noreply.github.com>
2020-09-19 08:53:45 -0500

    no more LFS

--- src/_data/pictures.js
+++
@@ -1,7 +0,0 @@
-module.exports = (url, alt = "Missing alt text") => {
-    return `<picture loading="lazy">
-  <source data-srcset="/assets/img/${url}?nf_resize=fit&w=700" media="(min-width: 1200px)">
-  <source data-srcset="/assets/img/${url}?nf_resize=fit&w=600" media="(min-width: 740px)">
-  <img data-src="/assets/img/${url}?nf_resize=fit&w=320" alt="${alt}" />
-</picture>`;
-};
